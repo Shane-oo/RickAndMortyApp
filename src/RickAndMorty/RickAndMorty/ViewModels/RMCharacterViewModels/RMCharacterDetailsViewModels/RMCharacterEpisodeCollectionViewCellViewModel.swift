6b5f3a3faa6aa325681161bb107d7e7e0e85fc69
Shane <shanemonck11@hotmail.com>
2023-07-09 16:44:45 +0800

    Implemented publish and subscribe pattern for fetching episodes

--- src/RickAndMorty/RickAndMorty/ViewModels/RMCharacterViewModels/RMCharacterDetailsViewModels/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ src/RickAndMorty/RickAndMorty/ViewModels/RMCharacterViewModels/RMCharacterDetailsViewModels/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -7,12 +7,74 @@
 
 import Foundation
 
+// publish and subscribe pattern
+protocol RMEpisodeDataRender {
+    var name: String { get }
+    var air_date: String { get }
+    var episode: String { get }
+}
+
 final class RMCharacterEpisodeCollectionViewCellViewModel {
-    private let episodeId: Int
+    private let episodeId: String
+    
+    private var isFetching = false
+    
+    private var dataBlock: ((RMEpisodeDataRender) -> Void)?
+    
+    private var episode: RMEpisodeModel? {
+        didSet {
+            guard let model = episode else {
+                return
+            }
+            dataBlock?(model)
+        }
+    }
+    
+    public var episodeRequest : RMRequestBuilder {
+        return RMRequestBuilder(endpoint: .episode,
+        pathComponents: [episodeId])
+    }
+    // MARK: - Init
     
     init (episodeUrl: URL?) {
-        // bad handling but i want to extract the episode id from url
-        self.episodeId = Int(episodeUrl?.lastPathComponent ?? "") ?? -1
+        guard let episodeId = episodeUrl?.lastPathComponent else {
+            self.episodeId = ""
+            return
+        }
+        self.episodeId = episodeId
+    }
+    
+    // MARK: - Public
+    
+    public func registerForData(_ block: @escaping (RMEpisodeDataRender) -> Void){
+        self.dataBlock = block
+    }
+    
+    public func fetchEpisode(){
+        guard !isFetching else {
+            // if we have it already give it back with publish
+            if let model = episode {
+                dataBlock?(model)
+            }
+            return
+        }
+        
+        isFetching = true
+        
+        RMService.shared.execute(
+            episodeRequest,
+            expecting: RMEpisodeModel.self
+        ) { [weak self] result in
+            switch result {
+            case .success(let response):
+                
+                DispatchQueue.main.async {
+                    self?.episode = response
+                }
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
     }
     
 }
